61d2cb378ecf90984e98745e33b2fecf1e8dccda
Charles Ueltschey <chaseuelt@gmail.com>
2025-09-17 15:43:19 -0500

    Loop to get all samples from UHD

--- uuagent/src/rf_uhd.cpp
+++ uuagent/src/rf_uhd.cpp
@@ -32,10 +32,17 @@ uuagent_error_e RF_UHD::collect_iq_data(const all_args_t &args) {
     stream_cmd.num_samps  = args.rf.num_samples;
     stream_cmd.stream_now = true;
     rx_stream->issue_stream_cmd(stream_cmd);
-
-    // Receive samples
-    size_t received_samples =
-        rx_stream->recv(buffer.data(), args.rf.num_samples, md);
+    size_t total_received = 0;
+    while (total_received < args.rf.num_samples) {
+	size_t n = rx_stream->recv(
+	buffer.data() + total_received,
+	args.rf.num_samples - total_received, md, 3.0);
+	if (md.error_code != uhd::rx_metadata_t::ERROR_CODE_NONE) {
+		std::cerr << "Error receiving samples: " << md.strerror() << std::endl;
+		return UUAGENT_SAMPLE_ERROR;
+	}
+	total_received += n;
+    }
 
     // Check for errors
     if (md.error_code != uhd::rx_metadata_t::ERROR_CODE_NONE) {
@@ -51,7 +58,7 @@ uuagent_error_e RF_UHD::collect_iq_data(const all_args_t &args) {
       return UUAGENT_FILE_ERROR;
     }
     outfile.write(reinterpret_cast<const char *>(buffer.data()),
-                  received_samples * sizeof(std::complex<float>));
+                  total_received * sizeof(std::complex<float>));
     outfile.close();
 
     std::cout << "IQ Data saved to " << args.rf.output_file << std::endl;
